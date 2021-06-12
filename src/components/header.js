b180b9b0b4309ebac8f12e590e1944de48235426
stkirk <shaanekirk@gmail.com>
2021-06-12 11:22:55 -0400

    completed header component with element hierarchy and their attached classes and content

--- src/components/header.js
+++ src/components/header.js
@@ -10,16 +10,38 @@ const Header = (title, date, temp) => {
   //    <h1>{ title }</h1>
   //    <span class="temp">{ temp }</span>
   //  </div>
-  //
-}
+
+  //create header elements
+  const headerDiv = document.createElement("div");
+  const dateSpan = document.createElement("span");
+  const h1 = document.createElement("h1");
+  const tempSpan = document.createElement("span");
+
+  //add classes and content to new elements
+  headerDiv.classList.add("header");
+  dateSpan.classList.add("date");
+  dateSpan.textContent = date;
+  h1.textContent = title;
+  tempSpan.classList.add("temp");
+  tempSpan.textContent = temp;
+
+  //append new elements to headerDiv to create markup
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(h1);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
+};
 
 const headerAppender = (selector) => {
+  console.log("the script tag worked");
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+};
 
-export { Header, headerAppender }
+export { Header, headerAppender };
